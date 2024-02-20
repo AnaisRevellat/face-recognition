1b138e351c050377dfe11ebd4b1f9edc542c59db
Hexia <you@example.com>
2024-02-20 16:36:19 +0100

    fixing parallax logo tilt and css

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Tilt } from "react-tilt";
+import styles from "./logo.module.css";
+import fRecognition from '../../assets/facial_recognition.jpeg'
+
+
+
+
 
 const Logo = () => {
-    return (
-        <div></div>
-    )
-}
+    
+  return (
+    <Tilt className={styles.tilt} options={{max: 50}} >
+      <div className={styles.logo_box}>
+        <img src={fRecognition} alt="facial recognition"/>
+      </div>
+    </Tilt>
+  );
+};
 
 export default Logo;
