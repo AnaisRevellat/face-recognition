b0e25cdc4f432aa8a90a1a24731c293c3fe9e954
Hexia <you@example.com>
2024-02-20 14:39:04 +0100

    navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './navigation.module.css'
+const Navigation = () => {
+  return (
+    <nav className={styles.main_nav}>
+      <p>Sign Out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
