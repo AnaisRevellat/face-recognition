9feeb6a562796f1677f9e3d843eace125688e6ec
Hexia <you@example.com>
2024-02-20 14:49:02 +0100

    use of react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      {/* <Logo />
+    <Router>
+      <div className="App">
+        <Navigation />
+        {/* <Logo />
     <ImageLinkForm />
     <FaceRecognition /> */}
-    </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from './navigation.module.css'
 const Navigation = () => {
   return (
     <nav className={styles.main_nav}>
-      <p>Sign Out</p>
+      <Link to="/">Sign Out</Link>
     </nav>
   );
 };
